88631afb9e8741e9f35e1694792996976d9df839
Jonghyuk Jung <contact@jonghyuk.info>
2017-05-23 16:56:57 +0900

    Added basic contents for about page.

--- PointingTest/myapps/about/admin.py
+++ PointingTest/myapps/about/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from myapps.about.models import *
 
-# Register your models here.
+class IntroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content',)
+
+# register admin pages
+
+admin.site.register(Introduction, IntroAdmin)
--- PointingTest/myapps/about/views.py
+++ PointingTest/myapps/about/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from myapps.about.models import *
 
 def index(request):
+    intro = Introduction.objects.all()[0]
+
     qs = {
+        "intro": intro,
     }
 
     return render(request, 'about.html', qs)
